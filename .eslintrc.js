07a125dd390ac723780051bb0cb75608be6b5542
arvinxx <arvinx@foxmail.com>
2023-01-08 17:29:18 +0800

    :arrow_up: chore: 升级模板基础脚手架

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,1 @@
-module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
-  globals: {
-    ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
-    page: true,
-    REACT_APP_ENV: true,
-  },
-  rules: {
-    '@typescript-eslint/method-signature-style': 0,
-    '@typescript-eslint/no-shadow': 0,
-  },
-  ignorePatterns: ['*.js'],
-};
+module.exports = require('@umijs/lint/dist/config/eslint');
