df28128ac17436411f9c9cbf30cc8f1010ef7fdb
arvinxx <arvinx@foxmail.com>
2023-01-08 17:38:38 +0800

    :wrench: chore: 优化发布配置，不发布 npm 包

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,5 @@
-module.exports = {
-  extends: ['semantic-release-config-gitmoji'],
-};
+const createConfig = require('semantic-release-config-gitmoji/lib/createConfig');
+
+const config = createConfig({ npmPublish: false });
+
+module.exports = config;
